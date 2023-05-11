bd7ed956776958929ec077636b20ff6162dd1813
Timm Knape <timm.knape@mechatronic.de>
2023-05-11 17:59:21 +0200

    added 2nd unit-test

--- cur/run_tests.sh
+++ cur/run_tests.sh
@@ -4,12 +4,35 @@ rm -Rf tmp
 mkdir tmp
 cd tmp
 
-# [AKR2] Keine Deltas
+# [AKR1] Keine Deltas
 
 mkdir delta
-for l in $(../pw); do
-	print "not expecting line '$l'" >&2
+for l in $(../pw --deltas); do
+	echo "not expecting line '$l'" >&2
 	exit 10
 done
 
+# [AKR2] 2 Zeilen
+
+files=("1-first.patch" "2-2nd.patch")
+for f in ${files[*]}; do touch delta/$f; done
+
+i=0
+for l in $(../pw --deltas); do
+	if ((i == ${#files[@]})); then
+		echo "not expecting line '$l'" >&2
+		exit 10
+	fi
+	if [[ $l != ${files[$i]} ]]; then
+		echo "expecting '${files[$i]}' but got '$l'" >&2
+		exit 10
+	fi
+	((i++))
+done
+if ((i != ${#files[@]})); then
+	echo "not enough lines; got $i but expected ${#files[@]}" >&2
+	exit 10
+fi
+
+cd ..
 rm -Rf tmp
--- cur/src/pw.cpp
+++ cur/src/pw.cpp
@@ -1,3 +1,19 @@
+#include <filesystem>
+#include <iostream>
+#include <map>
+
 int main(int argc, const char *argv[]) {
+	std::map<int, std::string> deltas;
+	for (const auto &f : std::filesystem::directory_iterator { "delta" }) {
+		std::string p = f.path().filename().string();
+		int v = 0;
+		for (const char *c = p.c_str(); isdigit(*c); ++c) {
+			v = v * 10 + (*c - '0');
+		}
+		deltas[v] = p;
+	}
+	for (const auto &e : deltas) {
+		std::cout << e.second << '\n';
+	}
 	return 0;
 }
